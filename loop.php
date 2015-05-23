f09e5afba3f73c939851dae19f9602a26405e105
coder1 <macbook@MBP.fritz.box>
2015-05-23 18:27:31 +0200

    update

--- loop.php
+++ loop.php
@@ -1,15 +1,28 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <article class="post" <?php $color = get_post_meta($post->ID, 'color', true); ?><?php if (!empty($color)) {echo 'style="background: '; echo $color; echo ';"';}?>>
-
 <div class="post-body <?php $font_black = get_post_meta($post->ID, 'font_black', true); ?><?php if (!empty($font_black)) {echo 'font-black';}?><?php $font_white = get_post_meta($post->ID, 'font_white', true); ?><?php if (!empty($font_white)) {echo 'font-white';}?>">
 
 
+<?php $image = get_post_meta($post->ID, 'image', true); ?>
+<?php $imagemaxwidth = get_post_meta($post->ID, 'image-max-width', true); ?>
+
+<?php if (!empty($image)) {
+	echo '<div class="image-container">';
+	echo '<img src="';
+	echo $image;
+	echo '" alt="" ';
+	if (!empty($imagemaxwidth)) {
+		echo 'style="max-width:';
+		echo $imagemaxwidth;
+		echo 'px;"';
+	};
+	echo ' />'; 
+	echo '</div>';
+}?>
+
+
 <div class="entry-body">
-
-	<div class="container">
-
-	<div class="datestamp"><b <?php if (!empty($color)) {echo 'style="background: '; echo $color; echo ';"';}?>><?php the_date(); ?></b></div>
 
 		<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
@@ -17,34 +30,23 @@
 		<?php the_category(', '); ?>
 		</div>
 
-		<section class="entry">
+		<div class="description">
+			<?php the_content('more'); ?>
+		</div>
 
-			<div class="description">
-				<?php the_content('more'); ?>
-			</div>
-
-		</section>
-
-		<!--
-		<section class="metadata">
-		<?php the_tags('', ''); ?>
-		</section>
-
-		<div class="clear"></div>
-		-->
 		<?php edit_post_link(__('Edit', ''), '', ''); ?>
-
-	</div>
 
 </div>
 
 
 </div> <!-- post-body end-->
 
+
+
 </article> <!-- post end-->
 
 <?php endwhile; else: ?>
 
 
-<p>Sorry, no posts matched your criteria.</p>
+<p>404</p>
 <?php endif; ?>
