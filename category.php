50dc0a86cae9372bada86d659de4c4f76d2fc963
coder1 <macbook@MBP.fritz.box>
2015-06-21 16:07:03 +0200

    responsive-video-teaser

--- category.php
+++ category.php
@@ -1,11 +1,11 @@
-<?php get_header('category'); ?>
+<?php get_header(); ?>
 
 <div id="content">
 <?php include ('loop.php'); ?>
 </div>
 
 <div class="container">
-  <div class="navigation"><p><?php posts_nav_link(' ','&larr; back','forward &rarr;'); ?></p></div>
+	<div class="navigation"><p><?php posts_nav_link(' ','<i class="demo-icon icon-left-open">&#xe801;</i>Newer','Older<i class="demo-icon icon-right-open">&#xe800;</i>'); ?></p></div>
 </div>
 
 </div><!-- #main -->
