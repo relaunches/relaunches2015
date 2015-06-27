7b13af0eaf7a6c1a116d16325cf7025a01e68f2c
coder1 <macbook@MBP.fritz.box>
2015-06-27 17:51:06 +0200

    page-navigation

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 <?php include ('loop.php'); ?>
 </div>
 
-<div class="container">
-	<div class="navigation"><?php posts_nav_link(' ','<i class="demo-icon icon-left-open">&#xe801;</i>Newer','Older<i class="demo-icon icon-right-open">&#xe800;</i>'); ?></div>
+<div class="navigation">
+	<div class="navigation--prev-page">
+	<?php echo get_previous_posts_link('<i class="demo-icon icon-left-open">&#xe801;</i>Newer'); ?>
+	</div>
+
+	<div class="navigation--next-page">
+	<?php echo get_next_posts_link('Older<i class="demo-icon icon-right-open">&#xe800;</i>'); ?>
+	</div>
 </div>
 
 </div><!-- #main -->
